87bff3cefbf07fbd1dcaed3d573dce28cd11c8c8
Yanrong <suga_e@outlook.com>
2022-05-12 05:11:31 +0100

    part1

---
+++ yxw257_assignment2/Part 1/yxw257_assignment2_part1_Q1_2.m
@@ -0,0 +1,62 @@
+%% 
+% *Question 1.2*
+
+clear all; close all; clc; 
+%% 
+% There are various algorithms which detect different kinds of features. An 
+% image of the School of Computer Science of UoB is provided. 
+% 
+% Plot on a 2×3 sub-plotted figure, the 100 strongest features when using the 
+% 
+% Minimum Eigenvalue, SURF, KAZE, FAST, ORB and the Harris-Stephens algorithms.
+% 
+% Include this sub-figure in your report and ensure that when the MATLAB file 
+% executes, it appears as a sub-figure. Save and include the MATLAB sub-figure 
+% in your submission. 
+
+I3 = imread("Q_1_2/UoB_CS_building.png");
+I = rgb2gray(I3);
+num = 100;
+fig = figure;
+
+
+points = detectMinEigenFeatures(I);
+subplot(2,3,1);
+imshow(I); hold on;
+plot(points.selectStrongest(num));
+hold off;
+
+points = detectSURFFeatures(I);
+subplot(2,3,2);
+imshow(I); hold on;
+plot(points.selectStrongest(num));
+hold off;
+
+points = detectKAZEFeatures(I);
+subplot(2,3,3);
+imshow(I); hold on;
+plot(points.selectStrongest(num));
+hold off;
+
+points = detectFASTFeatures(I);
+subplot(2,3,4);
+imshow(I); hold on;
+plot(points.selectStrongest(num));
+hold off;
+
+points = detectORBFeatures(I);
+subplot(2,3,5);
+imshow(I); hold on;
+plot(points.selectStrongest(num));
+hold off;
+
+points = detectHarrisFeatures(I);
+subplot(2,3,6);
+imshow(I); hold on;
+plot(points.selectStrongest(num));
+hold off;
+
+%%
+% export image as png file
+exportgraphics(fig, "yxw257_assignment2_part1_Q1_2.png", BackgroundColor="none", Resolution=600);
+savefig(fig, "yxw257_assignment2_part1_Q1_2.fig");
---
+++ yxw257_assignment2/Part 1/yxw257_assignment2_part1_Q1_3.m
@@ -0,0 +1,39 @@
+%% 
+% *Question 1.3*
+
+clear all; close all; clc; 
+%% 
+% Use SURF features to align B.jpg and C.jpg images to A.jpg. Find matching 
+% features and estimate the transformation between the images. Apply the calculated 
+% transformation on the images to generate the recovered images (expected results 
+% are shown below). Save and submit the recovered images
+
+IA = rgb2gray(imread("Q_1_3/A.jpg"));
+points_a = detectSURFFeatures(IA);
+[feat_a, vp_a] = extractFeatures(IA, points_a);
+
+pairs = ["B", "C"];
+
+for i=pairs
+    IB = rgb2gray(imread("Q_1_3/"+i+".jpg"));
+    points_b = detectSURFFeatures(IB);
+    [feat_b, vp_b] = extractFeatures(IB, points_b);
+    
+    index_pairs = matchFeatures(feat_a, feat_b,"MatchThreshold",15,'MaxRatio',1,'Unique',true);
+    matched_a = vp_a(index_pairs(:,1));
+    matched_b = vp_b(index_pairs(:,2));
+    
+    tform = estimateGeometricTransform2D(matched_b,matched_a,"similarity");
+    
+    outputView = imref2d(size(IA));
+    Ir = imwarp(IB,tform,'OutputView',outputView);
+
+    I = montage([IA, Ir]);
+    fig = figure ;
+    imshow(I.CData);
+    title("A<-"+i);
+
+    % export image as png file
+    exportgraphics(fig, "yxw257_assignment2_part1_A"+i+".png ", ...
+        BackgroundColor="none", Resolution=600);
+end
